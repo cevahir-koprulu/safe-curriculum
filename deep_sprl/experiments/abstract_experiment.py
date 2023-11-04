a3537c7b49c88495655ee7c53294be84e7a8faa3
cevahir-koprulu <cevahir.koprulu@hotmail.com>
2023-11-04 17:38:46 -0500

    PPO/SAC from sb3 to omnisafe, removed exp callback

--- deep_sprl/experiments/abstract_experiment.py
+++ deep_sprl/experiments/abstract_experiment.py
@@ -3,17 +3,12 @@ import time
 import torch
 import random
 import pickle
+import omnisafe
 import numpy as np
 from abc import ABC, abstractmethod
 from enum import Enum
 from deep_sprl.util.parameter_parser import create_override_appendix
-from deep_sprl.teachers.spl import SelfPacedTeacherV2
 
-from stable_baselines3.sac import SAC
-from stable_baselines3.ppo import PPO
-from stable_baselines3.common.vec_env import VecEnv, DummyVecEnv
-from stable_baselines3.ppo.policies import MlpPolicy as PPOMlpPolicy
-from stable_baselines3.sac.policies import MlpPolicy as SACMlpPolicy
 
 def set_seed(seed):
     random.seed(seed)
@@ -104,9 +99,8 @@ class CurriculumType(Enum):
 
 class AgentInterface(ABC):
 
-    def __init__(self, learner, obs_dim, device):
+    def __init__(self, learner, device):
         self.learner = learner
-        self.obs_dim = obs_dim
         self.device = device
 
     def estimate_value(self, inputs):
@@ -117,7 +111,7 @@ class AgentInterface(ABC):
         pass
 
     @abstractmethod
-    def mean_policy_std(self, cb_args, cb_kwargs):
+    def mean_policy_std(self):
         pass
 
     @abstractmethod
@@ -125,29 +119,25 @@ class AgentInterface(ABC):
         pass
 
     def save(self, log_dir):
-        self.learner.save(os.path.join(log_dir, "model"))
+        raise NotImplementedError("Saving not implemented: Omnisafe logger does this automatically")
+        # self.learner.save(os.path.join(log_dir, "model"))
 
 
 class SACInterface(AgentInterface):
 
-    def __init__(self, learner, obs_dim, device):
-        super().__init__(learner, obs_dim, device)
+    def __init__(self, learner, device):
+        super().__init__(learner, device)
 
     def estimate_value_internal(self, inputs):
-        return np.squeeze(self.learner.sess.run([self.learner.step_ops[6]], {self.learner.observations_ph: inputs}))
+        action, value_r, value_c, log_prob = self.learner._actor_critic.step(inputs, deterministic=False)
+        return value_r.detach().cpu().numpy()  
 
     def get_action(self, observations):
-        flat_obs = np.reshape(observations, (-1, observations.shape[-1]))
-        flat_acts = self.learner.predict(flat_obs, deterministic=False)[0]
-        acts = np.reshape(flat_acts, (observations.shape[0:-1]) + (-1,))
-        return acts
+        action, value_r, value_c, log_prob = self.learner._actor_critic.step(inputs, deterministic=False)
+        return action.detach().cpu().numpy()
 
-    def mean_policy_std(self, cb_args, cb_kwargs):
-        if "infos_values" in cb_args[0] and len(cb_args[0]["infos_values"]) > 0:
-            return cb_args[0]["infos_values"][4]
-        else:
-            return np.nan
-
+    def mean_policy_std(self):
+        return self.learner._actor_critic.actor.std.detach().cpu().numpy()
 
 class PPOInterface(AgentInterface):
 
@@ -156,20 +146,15 @@ class PPOInterface(AgentInterface):
         self.grad_fn = []
 
     def estimate_value_internal(self, inputs):
-        vals = self.learner.policy.predict_values(torch.from_numpy(inputs).to(
-            self.device))
-        return np.squeeze(vals.detach().cpu().numpy())
+        action, value_r, value_c, log_prob = self.learner._actor_critic.step(inputs, deterministic=False)
+        return value_r.detach().cpu().numpy() 
 
     def get_action(self, observations):
-        flat_obs = np.reshape(observations, (-1, observations.shape[-1]))
-        flat_acts = self.learner.predict(flat_obs, deterministic=False)[0]
-        return np.reshape(flat_acts, (observations.shape[0:-1]) + (-1,))
+        action, value_r, value_c, log_prob = self.learner._actor_critic.step(inputs, deterministic=False)
+        return action.detach().cpu().numpy()
 
-    def mean_policy_std(self, cb_args, cb_kwargs):
-        std_th = self.learner.policy.get_distribution(torch.zeros((1, self.obs_dim)).to(
-            self.device)).distribution.stddev[0]
-        return np.mean(std_th.detach().cpu().numpy())
-
+    def mean_policy_std(self):
+        return self.learner._actor_critic.actor.std.detach().cpu().numpy()
 
 class SACEvalWrapper:
 
@@ -177,8 +162,7 @@ class SACEvalWrapper:
         self.model = model
 
     def step(self, observation, state=None, deterministic=False):
-        return self.model.predict(observation, state=state, deterministic=deterministic)[0]
-
+        return self.model._actor_critic.step(observation, deterministic=deterministic)[0]
 
 class PPOEvalWrapper:
 
@@ -186,12 +170,7 @@ class PPOEvalWrapper:
         self.model = model
 
     def step(self, observation, state=None, deterministic=False):
-        if len(observation.shape) == 1:
-            observation = observation[None, :]
-            return self.model.predict(observation, state=state, deterministic=deterministic)[0][0, :]
-        else:
-            return self.model.predict(observation, state=state, deterministic=deterministic)[0]
-
+        return self.model._actor_critic.step(observation, deterministic=deterministic)[0]
 
 class Learner(Enum):
     PPO = 1
@@ -199,9 +178,9 @@ class Learner(Enum):
 
     def __str__(self):
         if self.ppo():
-            return "ppo"
+            return "PPO"
         else:
-            return "sac"
+            return "SAC"
 
     def ppo(self):
         return self.value == Learner.PPO.value
@@ -210,27 +189,20 @@ class Learner(Enum):
         return self.value == Learner.SAC.value
 
     def create_learner(self, env, parameters):
-        if self.ppo() and not issubclass(type(env), VecEnv):
-            env = DummyVecEnv([lambda: env])
-
+        # Be careful:
+        # - env should be env_id
+        # - parameters should be a custom_cfg dict
+        model = omnisafe.Agent(self.__str__(), env, parameters)
         if self.ppo():
-            model = PPO(PPOMlpPolicy, env, **parameters["common"], **parameters[str(self)])
-            interface = PPOInterface(model, env.observation_space.shape[0], parameters["common"]["device"])
+            interface = PPOInterface(model, model._device)
         else:
-            model = SAC(SACMlpPolicy, env, **parameters["common"], **parameters[str(self)])
-            interface = SACInterface(model, env.observation_space.shape[0], parameters["common"]["device"])
-
+            interface = SACInterface(model, model._device)
         return model, interface
 
     def load(self, path, env, device):
-        if self.ppo():
-            return PPO.load(path, env=env, device=device)
-        else:
-            return SAC.load(path, env=env, device=device)
+        raise NotImplementedError("Loading not implemented: Omnisafe evaluate does this/")
 
     def load_for_evaluation(self, path, env, device):
-        if self.ppo() and not issubclass(type(env), VecEnv):
-            env = DummyVecEnv([lambda: env])
         model = self.load(path, env, device)
         if self.sac():
             return SACEvalWrapper(model)
@@ -245,74 +217,6 @@ class Learner(Enum):
             return Learner.SAC
         else:
             raise RuntimeError("Invalid string: '" + string + "'")
-
-
-class ExperimentCallback:
-
-    def __init__(self, log_directory, learner, env_wrapper, save_interval=5, step_divider=1):
-        self.log_dir = os.path.realpath(log_directory)
-        self.learner = learner
-        self.env_wrapper = env_wrapper
-        self.save_interval = save_interval
-        self.algorithm_iteration = 0
-        self.step_divider = step_divider
-        self.iteration = 0
-        self.last_time = None
-        self.format = "   %4d    | %.1E |   %3d    |  %.2E  |  %.2E  |  %.2E   "
-        if self.env_wrapper.teacher is not None:
-            if isinstance(self.env_wrapper.teacher, SelfPacedTeacherV2):
-                context_dim = self.env_wrapper.teacher.context_dim
-                text = "| [%.2E"
-                for i in range(0, context_dim - 1):
-                    text += ", %.2E"
-                text += "] "
-                self.format += text + text
-        header = " Iteration |  Time   | Ep. Len. | Mean Reward | Mean Disc. Reward | Mean Policy STD "
-        if self.env_wrapper.teacher is not None:
-            if isinstance(self.env_wrapper.teacher, SelfPacedTeacherV2):
-                header += "|     Context mean     |      Context std     "
-        print(header)
-
-    def __call__(self, *args, **kwargs):
-        if self.algorithm_iteration % self.step_divider == 0:
-            data_tpl = (self.iteration,)
-
-            t_new = time.time()
-            dt = np.nan
-            if self.last_time is not None:
-                dt = t_new - self.last_time
-            data_tpl += (dt,)
-
-            mean_rew, mean_disc_rew, mean_length = self.env_wrapper.get_statistics()
-            data_tpl += (int(mean_length), mean_rew, mean_disc_rew)
-
-            data_tpl += (self.learner.mean_policy_std(args, kwargs),)
-
-            # Extra logging info
-            if isinstance(self.env_wrapper.teacher, SelfPacedTeacherV2):
-                context_mean = self.env_wrapper.teacher.context_dist.mean()
-                context_std = np.sqrt(np.diag(self.env_wrapper.teacher.context_dist.covariance_matrix()))
-                data_tpl += tuple(context_mean.tolist())
-                data_tpl += tuple(context_std.tolist())
-
-            print(self.format % data_tpl)
-
-            if self.iteration % self.save_interval == 0:
-                iter_log_dir = os.path.join(self.log_dir, "iteration-" + str(self.iteration))
-                os.makedirs(iter_log_dir, exist_ok=True)
-
-                with open(os.path.join(iter_log_dir, "context_trace.pkl"), "wb") as f:
-                    pickle.dump(self.env_wrapper.get_encountered_contexts(), f)
-
-                self.learner.save(iter_log_dir)
-                if self.env_wrapper.teacher is not None:
-                    self.env_wrapper.teacher.save(iter_log_dir)
-
-            self.last_time = time.time()
-            self.iteration += 1
-
-        self.algorithm_iteration += 1
-
 
 class AbstractExperiment(ABC):
     APPENDIX_KEYS = {"default": ["DISCOUNT_FACTOR", "STEPS_PER_ITER", "LAM"],
--- deep_sprl/teachers/abstract_teacher.py
+++ deep_sprl/teachers/abstract_teacher.py
@@ -1,13 +1,16 @@
 from typing import Any 
 from abc import ABC, abstractmethod
 
+import os
+import time
 import torch
+import pickle
 import numpy as np
-import gymnasium
 
 from gymnasium import spaces
 from omnisafe.envs.core import CMDP, make
 from deep_sprl.teachers.util import Buffer
+from deep_sprl.teachers.spl import SelfPacedTeacherV2
 
 class AbstractTeacher(ABC):
 
@@ -40,7 +43,14 @@ class BaseWrapper(CMDP):
         kwargs.pop('context_visible')
         kwargs.pop('context_post_processing')
         kwargs.pop('episodes_per_update')
-        
+
+        self.save_interval = kwargs['save_interval']
+        self.step_divider = kwargs['step_divider']
+        self.log_dir = kwargs['log_dir']
+        kwargs.pop('save_interval')
+        kwargs.pop('step_divider')
+        kwargs.pop('log_dir')
+
         super().__init__(env_id=env_id, **kwargs)
         self.env_id = env_id
         self.env_id_actual = env_id_actual
@@ -79,16 +89,80 @@ class BaseWrapper(CMDP):
         self.cur_context = None
         self.processed_context = None
         self.cur_initial_state = None
+        self.init_step_callback()
+
+    def init_step_callback(self):
+        self.algorithm_iteration = 0
+        self.iteration = 0
+        self.last_time = None
+        self.format = "   %4d    | %.1E |   %3d    |  %.2E  |  %.2E  "
+        # self.format = "   %4d    | %.1E |   %3d    |  %.2E  |  %.2E  |  %.2E   "
+        if self.teacher is not None:
+            if isinstance(self.teacher, SelfPacedTeacherV2):
+                context_dim = self.teacher.context_dim
+                text = "| [%.2E"
+                for i in range(0, context_dim - 1):
+                    text += ", %.2E"
+                text += "] "
+                self.format += text + text
+        header = " Iteration |  Time   | Ep. Len. | Mean Reward | Mean Disc. Reward "
+        # header = " Iteration |  Time   | Ep. Len. | Mean Reward | Mean Disc. Reward | Mean Policy STD "
+        if self.teacher is not None:
+            if isinstance(self.teacher, SelfPacedTeacherV2):
+                header += "|     Context mean     |      Context std     "
+        print(header)
 
     def done_callback(self, step, cur_initial_state, cur_context, discounted_reward, undiscounted_reward,
                       discounted_cost, undiscounted_cost):
         pass
+
+    def step_callback(self):
+        if self.algorithm_iteration % self.step_divider == 0:
+            data_tpl = (self.iteration,)
+
+            t_new = time.time()
+            dt = np.nan
+            if self.last_time is not None:
+                dt = t_new - self.last_time
+            data_tpl += (dt,)
+
+            mean_rew, mean_disc_rew, mean_length = self.get_statistics()
+            data_tpl += (int(mean_length), mean_rew, mean_disc_rew)
+
+            # data_tpl += (self.learner.mean_policy_std(),)
+
+            # Extra logging info
+            if isinstance(self.teacher, SelfPacedTeacherV2):
+                context_mean = self.teacher.context_dist.mean()
+                context_std = np.sqrt(np.diag(self.teacher.context_dist.covariance_matrix()))
+                data_tpl += tuple(context_mean.tolist())
+                data_tpl += tuple(context_std.tolist())
+
+            print(self.format % data_tpl)
+
+            if self.iteration % self.save_interval == 0:
+                iter_log_dir = os.path.join(self.log_dir, "iteration-" + str(self.iteration))
+                os.makedirs(iter_log_dir, exist_ok=True)
+
+                with open(os.path.join(iter_log_dir, "context_trace.pkl"), "wb") as f:
+                    pickle.dump(self.get_encountered_contexts(), f)
+
+                self.teacher.save(iter_log_dir)
+                # self.learner.save(iter_log_dir)
+                # if self.teacher is not None:
+                #     self.teacher.save(iter_log_dir)
+
+            self.last_time = time.time()
+            self.iteration += 1
+
+        self.algorithm_iteration += 1
 
     def step(self, action):
         obs, reward, cost, terminated, truncated, info = self._env.step(action)
         if self.context_visible:
             obs = torch.cat((obs, self.processed_context))
         self.update((obs, reward, cost, terminated, truncated, info))
+        self.step_callback()
         return obs, reward, cost, terminated, truncated, info
 
     def reset(
